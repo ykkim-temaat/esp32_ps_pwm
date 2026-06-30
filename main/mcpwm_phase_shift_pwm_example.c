169d8e7497f42aa93ed4ed9e46284477b0d12833
김윤기 <yoonki@yoonki-iMac.local>
2026-06-30 22:08:46 +0900

    2026-06-30; modified CMakeLists.txt

--- main/mcpwm_phase_shift_pwm_example.c
+++ main/mcpwm_phase_shift_pwm_example.c
@@ -27,6 +27,8 @@
 #include "led_strip.h"
 #include "soc/mcpwm_periph.h"
 
+#include "led_strip.h"
+
 #include "ps_pwm.h"
 
 void initialize_phase_shift_pwm()
@@ -116,10 +118,19 @@ void mcpwm_example_ps_pwm(void *arg)
     gpio_pad_select_gpio(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 #elif CONFIG_IDF_TARGET_ESP32S3
-    #define LED_PIN GPIO_NUM_48
-    led_strip_t *pStrip_a;
-    pStrip_a = led_strip_init(0, LED_PIN, 1);
-    pStrip_a->clear(pStrip_a, 50);
+    #define LED_PIN GPIO_NUM_48    
+    // 전역 변수 핸들 선언
+    led_strip_handle_t led_strip;
+    // 초기화 부분
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_PIN,
+        .max_leds = 1, // LED 개수
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    led_strip_clear(led_strip);
 #endif
 
     while (1) {
@@ -133,8 +144,8 @@ void mcpwm_example_ps_pwm(void *arg)
 #ifdef CONFIG_IDF_TARGET_ESP32
         gpio_set_level(LED_PIN, 1);
 #elif CONFIG_IDF_TARGET_ESP32S3         
-        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);            
-        pStrip_a->refresh(pStrip_a, 100);
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        led_strip_refresh(led_strip);
 #endif
         printf("pspwm_set_frequency 100kHz... \n");
         pspwm_set_frequency(MCPWM_UNIT_0, 100e3); // 100 kHz
@@ -142,7 +153,7 @@ void mcpwm_example_ps_pwm(void *arg)
         // vTaskDelay(3*configTICK_RATE_HZ);        
         vTaskDelay(3000 / portTICK_PERIOD_MS);
 #ifdef CONFIG_IDF_TARGET_ESP32
-        gpio_set_level(LED_PIN, 0);
+        led_strip_clear(led_strip);
 #elif CONFIG_IDF_TARGET_ESP32S3
             pStrip_a->clear(pStrip_a, 50);
 #endif
